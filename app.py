45c66e2e264ca6888f38bf7f2aec8ad149c63ad8
Abdurrahman Alowain <47498835+A-Owain@users.noreply.github.com>
2025-04-10 20:42:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -90,10 +90,10 @@ email = st.text_input("Email", "")
 phone = st.text_input("Phone", "")
 
 fonts = {
-    "ar_bold": load_font(..., 96),
-    "en_bold": load_font(..., 96),
-    "ar_regular": load_font(..., 72),
-    "en_regular": load_font(..., 72),
+    "ar_bold": load_font("assets/fonts/NotoSansArabic-SemiBold.ttf", 144),
+    "ar_regular": load_font("assets/fonts/NotoSansArabic-Regular.ttf", 96),
+    "en_bold": load_font("assets/fonts/PlusJakartaSans-Bold.ttf", 144),
+    "en_regular": load_font("assets/fonts/PlusJakartaSans-Regular.ttf", 96),
 }
 
 if all([ar_name, ar_title, en_name, en_title, email, phone]):
